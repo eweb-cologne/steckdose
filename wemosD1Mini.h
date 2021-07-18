7aebdfac836e801593d4b8374bcd105c4803719d
eweb-cologne <87609906+eweb-cologne@users.noreply.github.com>
2021-07-18 18:33:50 +0200

    Create wemosD1Mini.h

---
+++ wemosD1Mini.h
@@ -0,0 +1,14 @@
+#ifndef WEMOSD1MINI_H
+#define WEMOSD1MINI_H
+
+# Relais K1
+#define K1_PIN = 
+#define K1_ON = HIGH
+#define K1_OFF = LOW
+
+# Leuchtmelder P1
+#define P1_PIN = 
+#define P1_ON = HIGH
+#define P1_OFF = LOW
+
+#endif /* WEMOSD1MINI_H  */

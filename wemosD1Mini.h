6ec58b086633234baadb530c6f015112927500e9
eweb-cologne <87609906+eweb-cologne@users.noreply.github.com>
2021-07-18 18:38:37 +0200

    Update wemosD1Mini.h
    Ledbuildin

--- wemosD1Mini.h
+++ wemosD1Mini.h
@@ -2,12 +2,12 @@
 #define WEMOSD1MINI_H
 
 # Relais K1
-#define K1_PIN = 
+#define K1_PIN = D1
 #define K1_ON = HIGH
 #define K1_OFF = LOW
 
 # Leuchtmelder P1
-#define P1_PIN = 
+#define P1_PIN = LED_BUILTIN
 #define P1_ON = HIGH
 #define P1_OFF = LOW
 
